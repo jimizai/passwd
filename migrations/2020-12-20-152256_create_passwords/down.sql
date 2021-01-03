b92719974e6ce4e70cbe11dd3af294c8cf7a35eb
dyk <359743984@qq.com>
2021-01-03 23:43:55 +0800

    fix: bug fixed

--- migrations/2020-12-20-152256_create_passwords/down.sql
+++ migrations/2020-12-20-152256_create_passwords/down.sql
@@ -1,8 +1,1 @@
-CREATE TABLE `passwords` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `key` varchar(100) NOT NULL DEFAULT '',
-  `value` varchar(100) NOT NULL DEFAULT '',
-  `length` int(4) NOT NULL,
-  `type` varchar(20) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE passwords;
--- migrations/2020-12-20-152256_create_passwords/up.sql
+++ migrations/2020-12-20-152256_create_passwords/up.sql
@@ -1,1 +1,10 @@
-DROP TABLE passwords;
+CREATE TABLE `passwords` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `key` varchar(100) NOT NULL DEFAULT '',
+  `value` varchar(100) NOT NULL DEFAULT '',
+  `length` int(4) NOT NULL,
+  `type` varchar(20) NOT NULL DEFAULT '',
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
