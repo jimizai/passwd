8d728dab4a19b9a176aae5c1c56e8a75481045a3
dyk <359743984@qq.com>
2021-01-06 00:02:11 +0800

    feat: add global sync userinfo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, HashRouter as Router } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { syncUserInfo } from './store/actions';
 import Toast from './components/Toast/Toast';
 import Pages from './Pages';
 import Login from './pages/Login/Login';
 import NotFound from './pages/NotFound/NotFound';
 
 function App() {
+  const dispatch = useDispatch();
+  dispatch(syncUserInfo());
+
   return (
     <div>
       <Toast />
