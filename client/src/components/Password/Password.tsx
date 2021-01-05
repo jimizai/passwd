e78d46336cb43abdbd16ff7aeefbd35b2473cb36
dyk <359743984@qq.com>
2021-01-06 00:03:27 +0800

    feat: password component fixed

--- client/src/components/Password/Password.tsx
+++ client/src/components/Password/Password.tsx
@@ -1,15 +1,20 @@
-import React, { FC } from 'react';
-import { Grid, Typography, Button } from '@material-ui/core';
+import React, { FC, useState, useRef } from 'react';
+import { Grid, Typography, Button, Popover } from '@material-ui/core';
+import copy from 'copy-to-clipboard';
 import './Password.scss';
 
 interface P {
   id: number;
   title: string;
   value: string;
-  createAt: string;
+  createdAt: string;
+  onDelete: (id: number) => void;
 }
 
 const Password: FC<P> = props => {
+  const [open, setOpen] = useState<boolean>(false);
+  const anchorEl = useRef<any>(null);
+
   return (
     <div className='password-container'>
       <Grid container spacing={2}>
@@ -21,31 +26,71 @@ const Password: FC<P> = props => {
               </Typography>
               <Typography variant='body2' gutterBottom>
                 {props.value}
+                <Button
+                  color='primary'
+                  size='small'
+                  onClick={() => {
+                    copy(props.value);
+                  }}
+                >
+                  Copy
+                </Button>
               </Typography>
               <Typography variant='body2' color='textSecondary'>
                 ID: {props.id}
               </Typography>
               <Typography variant='body2' color='textSecondary'>
-                CreateAt: {props.createAt}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant='body2' style={{ cursor: 'pointer' }}>
-                <Button
-                  variant='contained'
-                  color='primary'
-                  onClick={() => {
-                    console.log('edit');
-                  }}
-                >
-                  Edit
-                </Button>
+                CreatedAt: {props.createdAt}
               </Typography>
             </Grid>
           </Grid>
           <Grid item>
             <Typography variant='subtitle1'>
-              <Button>Remove</Button>
+              <Popover
+                open={open}
+                anchorEl={anchorEl.current}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'center'
+                }}
+                transformOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center'
+                }}
+              >
+                <div className='password-content'>
+                  <div>Are you sure to delete this task?</div>
+                  <div className='password-bottom'>
+                    <Button
+                      size='small'
+                      onClick={() => {
+                        setOpen(false);
+                      }}
+                    >
+                      No
+                    </Button>
+                    <Button
+                      color='primary'
+                      size='small'
+                      onClick={() => {
+                        props.onDelete(props.id);
+                        setOpen(false);
+                      }}
+                    >
+                      Yes
+                    </Button>
+                    <div className='clearfix' />
+                  </div>
+                </div>
+              </Popover>
+              <Button
+                ref={anchorEl}
+                onClick={() => {
+                  setOpen(true);
+                }}
+              >
+                Remove
+              </Button>
             </Typography>
           </Grid>
         </Grid>
