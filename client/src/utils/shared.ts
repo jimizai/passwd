38bc0648dace2b9c37c5b933474152c57efe51d3
dyk <359743984@qq.com>
2021-01-06 00:05:41 +0800

    feat: add random string fn

--- client/src/utils/shared.ts
+++ client/src/utils/shared.ts
@@ -2,3 +2,11 @@ export const makeRegxMatch = (regExp: RegExp) => (str: string) => regExp.test(st
 
 const emailRegx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const matchEmail = makeRegxMatch(emailRegx);
+
+export const randomStr = () => {
+  return (
+    Math.random().toString(36).substring(2) +
+    Math.random().toString(36).substring(2) +
+    Math.random().toString(36).substring(2)
+  );
+};
