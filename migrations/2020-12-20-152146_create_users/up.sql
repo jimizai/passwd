2417300a0826d71fa25c0e7348a6bfee05acc579
dyk <359743984@qq.com>
2021-01-13 23:42:31 +0800

    fix: use password hash replace string

--- migrations/2020-12-20-152146_create_users/up.sql
+++ migrations/2020-12-20-152146_create_users/up.sql
@@ -2,6 +2,7 @@ CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL DEFAULT '',
   `email` varchar(100) NOT NULL DEFAULT '',
-  `password` varchar(100) NOT NULL DEFAULT '',
+  `salt` BLOB NOT NULL,
+  `password` BLOB NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
